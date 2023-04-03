b794235bd6ea6095c8f50dda9924a4dbf22b7bbf
iftekharul-islam <iftekharulislam.cse@gmail.com>
2023-04-04 03:12:05 +0600

    migration create and update

--- src/views/Adjustment/list/productionRejectscolumns.js
+++ src/views/Adjustment/list/productionRejectscolumns.js
@@ -14,49 +14,56 @@ export const productionRejectscolumns = [
   {
     name: "Stock Number",
     sortable: false,
-    minWidth: "120px",
+    minWidth: "150px",
     sortField: "stock_no_unique",
-    selector: (row) => row.stock_no_unique,
-    cell: (row) => <span>{row.stock_no_unique}</span>,
+    selector: (row) => row?.item?.inventoryunit,
+    cell: (row) =>
+    // { [...row?.item?.inventoryunit].map((listValue, index) => {
+    //   return (
+    //       <span key={index}>{listValue?.stock_no_unique}</span>
+    //   )
+    // }) }
+        <span>{row?.item?.inventoryunit[0].stock_no_unique}</span>
+        // <span>{row?.item?.inventoryunit[1].stock_no_unique}</span>
   },
   {
-    name: "Description",
+    name: "Item ID",
     sortable: false,
-    minWidth: "320px",
-    sortField: "stock_name_discription",
-    selector: (row) => row.stock_name_discription,
+    minWidth: "100px",
+    sortField: "item_id",
+    selector: (row) => row.item_id,
     cell: (row) => (
       <span>
-        {row?.stock_name_discription ?? "Inventory not found"}:
+        {row?.item_id ?? "--"}
       </span>
     ),
   },
   {
-    name: "Quantity",
+    name: "Status",
     sortable: true,
     minWidth: "100px",
-    sortField: "quantity",
-    selector: (row) => row.quantity,
+    sortField: "graphic_status",
+    selector: (row) => row.graphic_status,
     cell: (row) => (
-      <span className="text-capitalize">{row?.quantity}</span>
+      <span className="text-capitalize">{row?.graphic_status}</span>
     ),
   },
   {
-    name: "Type",
+    name: "Reason",
     minWidth: "150px",
     sortField: "type",
-    selector: (row) => row.type,
+    selector: (row) => row.rejection_message,
     cell: (row) => (
-      <span className="text-capitalize">{row?.type}</span>
+      <span className="text-capitalize">{row?.rejection_reason_info?.rejection_message}</span>
     ),
   },
   {
-    name: "Note",
+    name: "Message",
     minWidth: "300px",
     sortField: "note",
-    selector: (row) => row.note,
+    selector: (row) => row.rejection_message,
     cell: (row) => (
-      <span className="text-capitalize">{row?.note ?? '--'}</span>
+      <span className="text-capitalize">{row?.rejection_message ?? '--'}</span>
     ),
   },
   {
@@ -66,6 +73,15 @@ export const productionRejectscolumns = [
     selector: (row) => row.user,
     cell: (row) => (
       <span className="text-capitalize">{row?.user?.name}</span>
+    ),
+  },
+  {
+    name: "Quantity",
+    minWidth: "100px",
+    sortField: "user",
+    selector: (row) => row.reject_qty,
+    cell: (row) => (
+        <span className="text-capitalize">{row?.reject_qty}</span>
     ),
   },
 ];
