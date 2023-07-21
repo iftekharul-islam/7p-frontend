6be70ae9150e7aaef8060854404f6f47f7d962df
imrul18 <imrul.afnan18@gmail.com>
2023-07-21 23:42:47 +0600

    update wap

---
+++ dist/assets/index.d490fe9a.js
@@ -0,0 +1,1 @@
+import{a as r,r as c,cp as i,j as p}from"./index.9d6077eb.js";import{a as o}from"./App.6b53c8d0.js";const n=()=>{var a;const e=o(),s=r(t=>t.permissions);return c.exports.useEffect(()=>{e(i())},[]),p("div",{className:"app-user-list",children:(a=s==null?void 0:s.data)==null?void 0:a.map(t=>{})})};export{n as default};
