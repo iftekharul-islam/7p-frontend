c072b07d2b44e04f7a297cfaf856b59854141fa5
imrul18 <imrul.afnan18@gmail.com>
2023-08-27 21:12:13 +0600

    task complete

--- dist/assets/index.697d8e99.js
+++
@@ -1,1 +0,0 @@
-import{a as r,r as c,cu as i,j as o}from"./index.e509d298.js";import{a as p}from"./App.fa99906d.js";const n=()=>{var a;const e=p(),s=r(t=>t.permissions);return c.exports.useEffect(()=>{e(i())},[]),o("div",{className:"app-user-list",children:(a=s==null?void 0:s.data)==null?void 0:a.map(t=>{})})};export{n as default};
---
+++ dist/assets/index.c1492c61.js
@@ -0,0 +1,1 @@
+import{a as r,r as c,cu as i,j as o}from"./index.2802cfd2.js";import{a as p}from"./App.68a9a764.js";const n=()=>{var a;const e=p(),s=r(t=>t.permissions);return c.exports.useEffect(()=>{e(i())},[]),o("div",{className:"app-user-list",children:(a=s==null?void 0:s.data)==null?void 0:a.map(t=>{})})};export{n as default};
