a4dda9e65434616f1b9e391d2062a5cb684487b3
imrul18 <imrul.afnan18@gmail.com>
2023-09-07 01:04:15 +0600

    update for monogram

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 // const baseUrl = 'http://7p.test/api'
-const baseUrl = 'https://api-7p.viserx.dev/api'
+// const baseUrl = 'https://api-7p.viserx.dev/api'
+const baseUrl = 'https://7p.monogramonline.com/api'
 
 export default baseUrl
