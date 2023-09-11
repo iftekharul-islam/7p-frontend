eab9ac04f1c23d9eb90444a673bbe0c048820c23
imrul18 <imrul.afnan18@gmail.com>
2023-09-11 17:23:44 +0600

    update task icon

--- src/@core/layouts/components/navbar/NavbarUser.js
+++ src/@core/layouts/components/navbar/NavbarUser.js
@@ -1,6 +1,6 @@
 // ** Dropdowns Imports
 import { useEffect, useState } from "react";
-import { CheckSquare } from "react-feather";
+import { Send } from "react-feather";
 import { useDispatch } from "react-redux";
 import { getUserTasks } from "../../../../redux/navbar";
 import UserDropdown from "./UserDropdown";
@@ -27,13 +27,13 @@ const NavbarUser = () => {
 
   return (
     <ul className="nav navbar-nav align-items-center ms-auto">
-      <div className={taskData?.unread > 0 ? "blinking" : ""}>
-        <a href="/tasks">
-          <CheckSquare
-            size={22}
-            className="me-75"
-            color={taskData?.unread > 0 ? "red" : "black"}
-          />
+      <a href="/tasks" className="d-flex">
+        <Send
+          size={22}
+          className="me-75"
+          color={taskData?.unread > 0 ? "red" : "black"}
+        />
+        <div className={taskData?.unread > 0 ? "blinking" : ""}>
           {taskData?.unread > 0 ? (
             <span className="text-danger">
               {taskData?.unread} NEW TASK{taskData?.unread > 1 ? "S" : ""}
@@ -43,8 +43,8 @@ const NavbarUser = () => {
               {taskData?.all_msg} TASK{taskData?.all_msg > 1 ? "S" : ""}
             </span>
           ) : null}
-        </a>
-      </div>
+        </div>
+      </a>
       <UserDropdown />
     </ul>
   );
